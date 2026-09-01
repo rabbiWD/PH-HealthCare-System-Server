5c05676599cb8c8aeee75ce954085d62818bea74
Rabbi Khan <Krabby2244@gmail.com>
2026-09-02 03:06:30 +0600

    google login welcome message done

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -473,6 +473,21 @@ const googleLogin = async (payload: IGoogleLoginPayload) => {
 					},
 				},
 			});
+
+			const templatePath = path.join(process.cwd(), "src/app/template/welcome-email.ejs");
+
+	const templateData = {
+		name: user.name,
+	}
+
+	const html =await ejs.renderFile(templatePath, templateData)
+
+	await transporter.sendMail({
+		from: config.email_sender,
+		to: user.email,
+		subject: "Welcome to PH Healthcare System",
+		html
+	})
 		}
 	}
 
