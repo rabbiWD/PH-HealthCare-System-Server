089906577d2fdc9d888cb356d5487bca69037dc8
Rabbi Khan <Krabby2244@gmail.com>
2026-09-11 23:46:38 +0600

    bkash access token done

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ import z from "zod";
 import { redisClient } from "./app/lib/redis";
 import crypto from "crypto";
 import { UserRoutes } from "./app/module/user/uesr.routes";
+import { getBkashIdToken } from "./app/lib/bkash";
 
 const app: Application = express();
 
@@ -38,7 +39,13 @@ app.use("/api/v1/user", UserRoutes);
 app.get("/test", async (req: Request, res: Response, next: NextFunction)=>{
 	try{
 
-		const otp = crypto.randomInt(100000, 1000000)
+		// Bkash token generation
+		const grantIdTokenResult = await getBkashIdToken();
+		console.log("Bkash Grant ID Token Result:", grantIdTokenResult);
+
+
+	// OTP generation and storage in Redis
+		// const otp = crypto.randomInt(100000, 1000000)
 		
 		// await redisClient.set("forgot-password-otp:patient1@gmail.com", "123456", {
 		// 	expiration:{
@@ -52,7 +59,7 @@ app.get("/test", async (req: Request, res: Response, next: NextFunction)=>{
 	res.status(httpStatus.OK).json({
 		success: true,
 		message: "Welcome to PH Healthcare System Backend",
-		data: otp,
+		data: null,
 	});
 	}catch(error){
 		console.log(error)
